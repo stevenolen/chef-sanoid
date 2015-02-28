af61c788230846f9eaba8dbe1d04be3a2c824d86
Steve Nolen <technolengy@gmail.com>
2015-02-27 16:22:01 -0800

    do support os loop

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,11 @@ maintainer_email 'technolengy@gmail.com'
 license          'GPL v3'
 description      'Installs/Configures sanoid'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
+
+%w(centos debian redhat ubuntu freebsd).each do |os|
+  supports os
+end
 
 recipe 'default', 'Installs sanoid from github'
 
